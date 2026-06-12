72b6c9c86c0831cf5320feb75dfa2de793019436
eullon1234-creator <eullon1234@gmail.com>
2026-06-12 07:18:27 -0300

    6

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,15 @@
 const CACHE_NAME = 'epi-gel-v1';
+
+// Detecta o base path dinamicamente a partir do próprio sw.js
+const BASE = self.location.pathname.replace(/\/sw\.js$/, '') || '';
+
 const ASSETS = [
-  '/CONTROLE-DE-EPI-GEL/',
-  '/CONTROLE-DE-EPI-GEL/index.html',
-  '/CONTROLE-DE-EPI-GEL/favicon.svg',
-  '/CONTROLE-DE-EPI-GEL/manifest.json',
-  '/CONTROLE-DE-EPI-GEL/icon-192.png',
-  '/CONTROLE-DE-EPI-GEL/icon-512.png'
+  `${BASE}/`,
+  `${BASE}/index.html`,
+  `${BASE}/favicon.svg`,
+  `${BASE}/manifest.json`,
+  `${BASE}/icon-192.png`,
+  `${BASE}/icon-512.png`
 ];
 
 self.addEventListener('install', (e) => {
